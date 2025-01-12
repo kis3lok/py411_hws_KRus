23d828997d8f8c91a994b0a680dabeffdd513841
CinnamonBun69 <Real.godl664@gmail.com>
2025-01-12 22:53:27 +0300

    Finished the class, did the test.

---
+++ hw4/hw4test.py
@@ -0,0 +1,11 @@
+from hw4 import TxtFileHandler
+
+
+file_path = "test.txt"
+handler = TxtFileHandler(file_path)
+
+handler.write_file("Hello", "World", encoding="utf-8")
+
+handler.append_file("1111111Hello", "111111World", encoding="utf-8")
+
+print(handler.read_file(encoding="utf-8"))
