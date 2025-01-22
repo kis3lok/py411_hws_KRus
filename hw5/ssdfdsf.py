9bbad7941f0067222ab5da1a75a66c3675fcbd1a
CinnamonBun69 <Real.godl664@gmail.com>
2025-01-22 21:56:32 +0300

    Added __call__ and if name == main

--- hw5/ssdfdsf.py
+++ hw5/ssdfdsf.py
@@ -52,6 +52,46 @@ class ImageCompressor:
                     input_path = os.path.join(root, file)
                     output_path = os.path.splitext(input_path)[0] + '.heic'
                     self.compress_image(input_path, output_path)
+    def __call__(self, input_path: str, output_path: str) -> None:
+        """
+        Основная метод программы. Обрабатывает входной путь и запускает сжатие изображений.
+        :param input_path (str): Путь к исходному изображению или директории.
+        :param output_path (str): Путь для сохранения сжатого изображения.
+        :return: None
+        """
+        register_heif_opener()
+        input_path = input_path.strip('"')  # Удаляем кавычки, если они есть
+        output_path = output_path.strip('"')
+    
+        if os.path.exists(input_path):
+            if os.path.isfile(input_path):
+                # Если указан путь к файлу, обрабатываем только этот файл
+                print(f"Обрабатываем файл: {input_path}")
+                output_path = os.path.splitext(input_path)[0] + '.heic'
+                self.compress_image(input_path, output_path)
+            elif os.path.isdir(input_path):
+                # Если указан путь к директории, обрабатываем все файлы в ней
+                print(f"Обрабатываем директорию: {input_path}")
+                # Если директория сохранения не существует, создаем ее.
+                if not os.path.exists(output_path):
+                    os.makedirs(output_path)
+                self.process_directory(input_path)
+                # Функция process_directory рекурсивно обойдет все поддиректории
+                # и обработает все поддерживаемые изображения
+        else:
+            print("Указанный путь не существует")
+
+def main():
+    """
+    Функция для поулчения путей к файлам и запуска сжатия.
+    """
+    user_input: str = input("Введите путь к файлу или директории: ")
+    user_output: str = input("Введите путь для сохранения сжатого изображения(ий): ")
+    compressor = ImageCompressor(80)
+    compressor(user_input, user_output)
+
+if __name__ == "__main__":
+    main()
 
     
     
