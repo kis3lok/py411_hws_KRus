e5e576151f5c702cf691f22058ce702540148f67
CinnamonBun69 <Real.godl664@gmail.com>
2025-01-22 21:55:40 +0300

    Created two methods: compress and process

--- hw5/ssdfdsf.py
+++ hw5/ssdfdsf.py
@@ -27,5 +27,31 @@ class ImageCompressor:
         """
         self.__quality = quality
     
+    def compress_image(self, input_path: str, output_path: str) -> None:
+        """
+        Метод сжатия изображений.
+        :param input_path (str): Путь к исходному изображению.
+        :param output_path (str): Путь для сохранения сжатого изображения.
+        :return: None
+        """
+        with Image.open(input_path) as img:
+            img.save(output_path, "HEIF", quality=self.__quality)
+        print(f"Сжато: {input_path} -> {output_path}")
+    
+    
+    def process_directory(self, directory: str) -> None:
+        """
+        Метод обработки директории.
+        :param directory (str): Путь к директории.
+        :return: None
+        """
+        for root, _, files in os.walk(directory):
+            for file in files:
+                # Проверяем расширение файла
+                if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+                    input_path = os.path.join(root, file)
+                    output_path = os.path.splitext(input_path)[0] + '.heic'
+                    self.compress_image(input_path, output_path)
+
     
     
