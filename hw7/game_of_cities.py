ba88c21249d431c144623b6bcf28dc3ddc61a8a9
CinnamonBun69 <Real.godl664@gmail.com>
2025-02-10 15:40:21 +0300

    jsonfile class added

--- hw7/game_of_cities.py
+++ hw7/game_of_cities.py
@@ -21,4 +21,34 @@ class City:
     is_used: bool = False
 
 
+class JsonFile:
+    """
+    Класс для работы с JSON файлами
+    """
+    def __init__(self, path: str = "cities.json"):
+        """
+        Инициализания класса
+        :param path: Путь к файлу
+        """
+        self.path = path
+
+    def read_data(self):
+        """
+        Читает данные из файла
+        :return: Список словарей с данными
+        """
+        with open(self.path, "r", encoding="utf-8") as file:
+            return json.load(file)
+    
+    def write_data(self, *data):
+        """
+        Записывает данные в файл
+        :param data: Список словарей с данными
+        :return: None
+        """
+        with open(self.path, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+    
+    
+
 
