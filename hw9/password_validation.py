1e8ce0b26160d18583ad02343548fa31548170ff
CinnamonBun69 <Real.godl664@gmail.com>
2025-02-23 15:44:02 +0300

    password_validation.py: started the 2nd task - created password_validation func

--- hw9/password_validation.py
+++ hw9/password_validation.py
@@ -38,3 +38,35 @@ register_user("Aa1!aaaa")
 # register_user("Aaa1aaa")
 # register_user("AAAAA!1AAA")
 
+# 2
+import csv
+
+def password_validator(length: int = 8, nums: int = 1, uppercase: int = 1, lowercase: int = 1, special_chars: int = 1) -> Callable:
+    """
+    Обёртка декоратора для проврки пароля с аргументами
+    :param length: необходимая длина пароля
+    :param nums: необходимое количество цифр
+    :param uppercase: необходимое количество заглавных букв
+    :param lowercase: необходимое количество строчных букв
+    :param special_chars: необходимое количество специальных символов
+    :return: декоратор
+    """
+    def decorator(func: Callable) -> Callable:   
+        """
+        Декоратор для проверки пароля
+        :param func: функция, которую декорируем
+        :return: функция
+        """
+        def wrapper(username:str, passw: str):
+            upprcs = sum(1 for i in passw if i.isupper())
+            lwrcs = sum(1 for i in passw if i.islower())
+            spcl = sum(1 for i in passw if not i.isalnum())
+            nmbrs = sum(1 for i in passw if i.isdigit())
+            if len(passw) >= length and nmbrs >= nums and upprcs >= uppercase and lwrcs >= lowercase and spcl >= special_chars:
+                return func(username,passw )
+            else:
+                raise ValueError('The password is not valid, please check the requirements.')
+
+        return wrapper
+    return decorator
+    
