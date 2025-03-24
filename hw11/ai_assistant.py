fccc07d49fb6238d1a3da7c7ac4eb4e932534b89
CinnamonBun69 <Real.godl664@gmail.com>
2025-03-24 23:34:21 +0300

    added imagerequest class to ai_assistant

--- hw11/ai_assistant.py
+++ hw11/ai_assistant.py
@@ -42,4 +42,73 @@ class TextRequest:
         except Exception as e:
             raise Exception(f"Непредвиденная ошибка: {str(e)}")
 
+class ImageRequest:
+    """
+    Класс для отправки запросов, включающих изображение.
+    """
+    def __init__(self, api_key:str) -> None:
+        self.api_key = api_key
+        self.client = Mistral(api_key=self.api_key)
 
+    def _encode_image(self, image_path: str) -> bytes:
+        """
+        Кодирует изображение в Base64.
+        :param image_path: Путь к изображению
+        :return: Кодированное изображение
+        """
+        try:
+            with open(image_path, "rb") as image_file:
+                encoded_image = image_file.read()
+                return encoded_image
+        except FileNotFoundError:
+            raise Exception("Файл не был найден.")
+        except Exception as e:
+            raise Exception(f"Непредвиденная ошибка при чтении изображения: {e}")
+        
+    def send(self, text:str, image_path:str, model:str = "pixtral-12b-2409", history:list = []) -> dict:
+        """
+        Отправляет мультимедийный запрос нейронке и возвращает ответ.
+        :param text: Текст запроса пользователя
+        :param image_path: Путь к изображению
+        :param model: Модель для использования (по умолчанию pixtral-12b-2409)
+        :param history: История предыдущих сообщений
+        :return: Словарь с ответом и обновленной историей        
+        """
+
+        try:
+            image_bytes = self._encode_image(image_path)
+            
+            base64_image = base64.b64encode(image_bytes).decode('utf-8')
+            
+            image_message = {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": text
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}"
+                    }
+                ]
+            }
+            
+            messages = history + [image_message]
+            
+            chat_response = self.client.chat.complete(
+                model=model,
+                messages=messages
+            )
+            
+            result = chat_response.choices[0].message.content
+            
+            updated_history = messages + [{"role": "assistant", "content": result}]
+            
+            return {
+                "response": result,
+                "history": updated_history
+            }
+        except Exception as e:
+            raise Exception(f"Ошибка при обработке изображения: {e}")
+        
